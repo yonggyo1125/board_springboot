071a4bf24d2ddb35f631a8f3d5fddce19558d824
yonggyo1125 <yonggyo00@kakao.com>
2022-07-30 22:44:58 +0900

    쿼리 메서드 강의 작성

--- src/main/java/com/codefty/repository/BoardDataRepository.java
+++ src/main/java/com/codefty/repository/BoardDataRepository.java
@@ -1,9 +1,14 @@
 package com.codefty.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codefty.entity.BoardData;
 
 public interface BoardDataRepository extends JpaRepository<BoardData, Long> {
 	
+	List<BoardData> findBySubject(String subject);
+	
+	List<BoardData> findBySubjectOrContents(String subject, String contents);
 }
--- src/test/java/com/codefty/repository/BoardDataRepositoryTest.java
+++ src/test/java/com/codefty/repository/BoardDataRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.codefty.repository;
 
+import java.util.List;
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
@@ -29,5 +30,36 @@ class BoardDataRepositoryTest {
 		
 		BoardData savedBoardData = boardDataRepository.save(boardData);
 		System.out.println(savedBoardData);
+	}
+	
+	public void createBoardDatas() {
+		for(int i = 1; i <= 10; i++) {
+			BoardData boardData = new BoardData();
+			boardData.setSubject("게시글 제목" + i);
+			boardData.setContents("게시글 본문" + i);
+			boardData.setRegDt(LocalDateTime.now());
+			boardData.setModDt(LocalDateTime.now());
+			BoardData savedBoardData = boardDataRepository.save(boardData);
+		}
+	}
+	
+	@Test
+	@DisplayName("게시글 제목 조회 테스트")
+	public void findBySubjectTest() {
+		this.createBoardDatas();
+		List<BoardData> boardDatas = boardDataRepository.findBySubject("게시글 제목1");
+		for (BoardData boardData : boardDatas) {
+			System.out.println(boardData);
+		}
+	}
+	
+	@Test
+	@DisplayName("게시글 제목, 게시글 본문 or 테스트")
+	public void findBySubjectOrContentsTest() {
+		this.createBoardDatas();
+		List<BoardData> boardDatas = boardDataRepository.findBySubjectOrContents("게시글 제목1", "게시글 본문1");
+		for (BoardData boardData : boardDatas) {
+			System.out.println(boardData);
+		}
 	}
 }
