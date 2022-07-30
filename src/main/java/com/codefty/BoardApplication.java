3c363a43454962264a956b56e5eee248c4ed30ac
yonggyo1125 <yonggyo00@kakao.com>
2022-07-30 19:32:36 +0900

    Entity 설계하기 소스 작성 완료

--- src/main/java/com/codefty/BoardApplication.java
+++ src/main/java/com/codefty/BoardApplication.java
@@ -1,4 +1,4 @@
-package com.codefty.project1;
+package com.codefty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/codefty/entity/BoardData.java
@@ -0,0 +1,29 @@
+package com.codefty.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class BoardData {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id; // 게시글 번호
+	
+	@Column(nullable=false, length=50)
+	private String subject; // 게시글 제목
+	
+	@Lob
+	@Column(nullable=false)
+	private String contents; // 게시글 내용
+	
+	private LocalDateTime regDt; //등록시간
+	private LocalDateTime modDt; // 수정시간
+}
